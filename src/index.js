2545a7d46e8b3d6424665ac0c9ce73ab7c5f27b8
Alex Mos <alexander.moskovkin@devexpress.com>
2017-03-19 13:44:29 +0300

    It works!

--- src/index.js
+++ src/index.js
@@ -1,155 +1,133 @@
 /*global document*/
-var Selector = require('testcafe').Selector;
+import { Selector, ClientFunction } from 'testcafe';
 
-export default Selector(selector => {
-    /*const SUPPORTED_REACT_VERSION = 15;
+const getComponentInstance = ClientFunction(el => {
+    if (el.nodeType !== 1)
+        return null;
 
-    if (!window.React)
-        return document.querySelectorAll(selector);
+    for (var prop of Object.keys(el)) {
+        if (!/^__reactInternalInstance/.test(prop))
+            continue;
 
-    const reactVersion = parseInt(window.React.version.split('.')[0], 10);
-
-    if (reactVersion < SUPPORTED_REACT_VERSION)
-        throw new Error('testcafe-react-selectors supports React version 15.x and newer');
-*/
-    function reactSelect (compositeSelector) {
-        const foundComponents = [];
-        let foundInstance     = null;
-
-        function getComponentInstance (el) {
-            if (el.nodeType !== 1)
-                return null;
-
-            for (var prop of Object.keys(el)) {
-                if (!/^__reactInternalInstance/.test(prop))
-                    continue;
-
-                //NOTE: stateless component
-                if (!el[prop]._currentElement._owner)
-                    return null;
-
-                return el[prop]._currentElement._owner._instance;
-            }
-        }
-
-        function getComponentName (DOMNode) {
-            let reactComponentName     = null;
-            const reactComponentParent = foundInstance._reactInternalInstance._renderedComponent._hostNode;
-
-            if (reactComponentParent === DOMNode) {
-                //NOTE: IE hack
-                if (foundInstance.constructor.name)
-                    reactComponentName = foundInstance.constructor.name;
-                else {
-                    const matches = foundInstance.constructor.toString().match(/^function\s*([^\s(]+)/);
-
-                    reactComponentName = matches ? matches[1] : DOMNode.tagName.toLowerCase();
-                }
-            }
-            else {
-                reactComponentName = DOMNode.tagName.toLowerCase();
-                foundInstance      = null;
-            }
-
-            return reactComponentName;
-        }
-
-        function findDOMNode () {
-            if (typeof compositeSelector !== 'string')
-                throw new Error(`Selector option is expected to be a string, but it was ${typeof compositeSelector}.`);
-
-            var selectorIndex = 0;
-            var selectorElms  = compositeSelector
-                .split(' ')
-                .filter(el => !!el)
-                .map(el => el.trim());
-
-            function walk (node, cb) {
-                var searchResult      = cb(node);
-                var currSelectorIndex = selectorIndex;
-
-                if (searchResult)
-                    return searchResult;
-
-                node = node.firstChild;
-
-                while (node) {
-                    searchResult = walk(node, cb);
-
-                    if (searchResult)
-                        return searchResult;
-
-                    node          = node.nextSibling;
-                    selectorIndex = currSelectorIndex;
-                }
-            }
-
-            return walk(document.body, node => {
-                if (!(foundInstance = getComponentInstance(node)))
-                    return null;
-
-                const componentName = getComponentName(node);
-
-                if (selectorElms[selectorIndex] !== componentName)
-                    return null;
-
-                if (selectorIndex === selectorElms.length - 1)
-                    foundComponents.push(node);
-
-                selectorIndex++;
-
-                return null;
-            });
-        }
-
-        findDOMNode();
-
-        return foundComponents;
-    }
-
-    return reactSelect(selector);
-}).addCustomMethods({
-    getReact: (node, fn) => {
-        function copyReactObject (obj) {
-            var copiedObj = {};
-
-            for (var prop in obj) {
-                if (obj.hasOwnProperty(prop) && prop !== 'children')
-                    copiedObj[prop] = obj[prop];
-            }
-
-            return copiedObj;
-        }
-
-        function getComponentForDOMNode (el) {
-            if (!el || el.nodeType !== 1)
-                return null;
-
-            for (var prop of Object.keys(el)) {
-                if (!/^__reactInternalInstance/.test(prop))
-                    continue;
-
-                //NOTE: stateless component
-                if (!el[prop]._currentElement._owner)
-                    return null;
-
-                var foundInstance = el[prop]._currentElement._owner._instance;
-
-                if (foundInstance._reactInternalInstance._renderedComponent._hostNode !== el)
-                    return null;
-
-                return foundInstance;
-            }
-        }
-
-        var componentInstance = getComponentForDOMNode(node);
-
-        if (!componentInstance)
+        //NOTE: stateless component
+        if (!el[prop]._currentElement._owner)
             return null;
 
-        return fn({
-            state: copyReactObject(componentInstance.state),
-            props: copyReactObject(componentInstance.props)
-        });
+        return el[prop]._currentElement._owner._instance;
     }
 });
+
+const getComponentName = ClientFunction((DOMNode, instance) => {
+    let reactComponentName     = null;
+    const reactComponentParent = instance._reactInternalInstance._renderedComponent._hostNode;
+
+    if (reactComponentParent === DOMNode) {
+        //NOTE: IE hack
+        if (instance.constructor.name)
+            reactComponentName = instance.constructor.name;
+        else {
+            const matches = instance.constructor.toString().match(/^function\s*([^\s(]+)/);
+
+            reactComponentName = matches ? matches[1] : DOMNode.tagName.toLowerCase();
+        }
+    }
+    else
+        reactComponentName = DOMNode.tagName.toLowerCase();
+
+    return reactComponentName;
+});
+
+const getReactObj = ClientFunction(node => {
+    function copyReactObject (obj) {
+        const copiedObj = {};
+
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop) && prop !== 'children')
+                copiedObj[prop] = obj[prop];
+        }
+
+        return copiedObj;
+    }
+
+    function getComponentForDOMNode (el) {
+        if (!el || el.nodeType !== 1)
+            return null;
+
+        for (var prop of Object.keys(el)) {
+            if (!/^__reactInternalInstance/.test(prop))
+                continue;
+
+            //NOTE: stateless component
+            if (!el[prop]._currentElement._owner)
+                return null;
+
+            const foundInstance = el[prop]._currentElement._owner._instance;
+
+            if (foundInstance._reactInternalInstance._renderedComponent._hostNode !== el)
+                return null;
+
+            return foundInstance;
+        }
+    }
+
+    const componentInstance = getComponentForDOMNode(node);
+
+    if (!componentInstance)
+        return null;
+
+    return {
+        state: copyReactObject(componentInstance.state),
+        props: copyReactObject(componentInstance.props)
+    };
+});
+
+const filterByComponentName = ClientFunction((node, componentName) => {
+    const componentInstance = getComponentInstance(node);
+
+    if (!componentInstance)
+        return false;
+
+    return getComponentName(node, componentInstance) === componentName;
+}, { dependencies: { getComponentInstance, getComponentName } });
+
+
+export default function (compositeSelector, options) {
+    if (typeof compositeSelector !== 'string')
+        throw new Error(`Selector option is expected to be a string, but it was ${typeof compositeSelector}.`);
+
+    const filter = node => {
+        /* eslint-disable no-undef */
+        return filterByComponentName(node, selector);
+        /* eslint-enable no-undef */
+    };
+
+    const selectorChunks = compositeSelector.split(' ');
+
+    let res = Selector(() => {
+        // TODO: check react
+        // window.React can be undefined on the react page
+        //const SUPPORTED_REACT_VERSION = 15;
+
+        //if (!window.React && !window.__REACT_DEVTOOLS_GLOBAL_HOOK__)
+        //return document.querySelectorAll(selector);
+
+        //const reactVersion = parseInt(window.React.version.split('.')[0], 10);
+
+        //if (reactVersion < SUPPORTED_REACT_VERSION)
+        //throw new Error('testcafe-react-selectors supports React version 15.x and newer');
+        return document.querySelectorAll('*');
+    }, options)(compositeSelector);
+
+    res = res.filter(filter, { filterByComponentName, selector: selectorChunks[0] });
+
+    for (let chunkIdx = 1; chunkIdx < selectorChunks.length; chunkIdx++)
+        res = res.find(filter, { filterByComponentName, selector: selectorChunks[chunkIdx] });
+
+    const getReact = ClientFunction((node, fn) => {
+        const reactObj = getReactObj(node);
+
+        return reactObj ? fn(reactObj) : null;
+    }, { dependencies: { getReactObj } });
+
+    return res.addCustomMethods({ getReact });
+}
